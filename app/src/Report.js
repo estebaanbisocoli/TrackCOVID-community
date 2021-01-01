efbd3bed95cb2065fd738703dbdd3187dc065c4c
Esteban <estebaanbisocoli@gmail.com>
2020-12-31 21:01:18 -0300

    test report

--- app/src/Report.js
+++ app/src/Report.js
@@ -1,67 +1,84 @@
-import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import GetAppIcon from '@material-ui/icons/GetApp'
-import { withTheme } from '@material-ui/core/styles'
-import { Translation } from 'react-i18next'
-import reportIcon from './img/report-icon.png'
-import API from './api'
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import GetAppIcon from "@material-ui/icons/GetApp";
+import { withTheme } from "@material-ui/core/styles";
+import { Translation } from "react-i18next";
+import reportIcon from "./img/report-icon.png";
+import API from "./api";
 
 const initialState = {
   exposureStatus: false,
   loaded: false,
-  mode: 'default'
-}
+  mode: "default",
+};
 
 class Report extends React.Component {
-  constructor () {
-    super()
-    this.state = initialState
+  constructor() {
+    super();
+    this.state = initialState;
   }
 
-  async reset () {
-    this.setState(initialState)
+  async reset() {
+    this.setState(initialState);
   }
 
-  async downloadHistory () {
-    const checkpoints = await API.exportCheckpoints()
-    var dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(checkpoints))
-    var dlAnchorElem = document.getElementById('downloadAnchorElem')
-    dlAnchorElem.setAttribute('href', dataStr)
-    dlAnchorElem.setAttribute('download', 'checkpoints.json')
-    dlAnchorElem.click()
+  async downloadHistory() {
+    const checkpoints = await API.exportCheckpoints();
+    var dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(checkpoints));
+    navigator
+      .share({
+        files: [dataStr],
+        title: "checkpoints.json",
+        text: "visitas",
+      })
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e)); // var dlAnchorElem = document.getElementById('downloadAnchorElem')
+    // dlAnchorElem.setAttribute('href', dataStr)
+    // dlAnchorElem.setAttribute('download', 'checkpoints.json')
+    // dlAnchorElem.click()
   }
 
-  render () {
-    const { mode } = this.state
+  render() {
+    const { mode } = this.state;
     return (
-      <Grid
-        container
-      >
-        {
-          ((mode === 'default') && (
-            <Grid
-              container
-              direction='column'
-              justify='center'
-              alignItems='center'
+      <Grid container>
+        {mode === "default" && (
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <img
+              src={reportIcon}
+              width={200}
+              style={{ maxWidth: "80px", marginTop: 20 }}
+            />
+            <Typography style={{ marginTop: 25 }}>
+              <Translation>{(t) => t("aboutReportMessage")}</Translation>
+            </Typography>
+            <Button
+              onClick={this.downloadHistory.bind(this)}
+              variant="contained"
+              color="primary"
+              aria-label="add"
+              style={{ marginTop: 25 }}
             >
-              <img src={reportIcon} width={200} style={{ maxWidth: '80px', marginTop: 20 }} />
-              <Typography style={{ marginTop: 25 }}>
-                <Translation>{t => t('aboutReportMessage')}</Translation>
-              </Typography>
-              <Button onClick={this.downloadHistory.bind(this)} variant='contained' color='primary' aria-label='add' style={{ marginTop: 25 }}>
-                <GetAppIcon />
-                <Translation>{t => t('downloadHistoryButton')}</Translation>
-              </Button>
-              <a id='downloadAnchorElem' href='/' style={{ display: 'none' }}><Translation>{t => t('downloadHistoryButton')}</Translation></a>
-            </Grid>
-          ))
-        }
+              <GetAppIcon />
+              <Translation>{(t) => t("downloadHistoryButton")}</Translation>
+            </Button>
+            <a id="downloadAnchorElem" href="/" style={{ display: "none" }}>
+              <Translation>{(t) => t("downloadHistoryButton")}</Translation>
+            </a>
+          </Grid>
+        )}
       </Grid>
-    )
+    );
   }
 }
 
-export default withTheme(Report)
+export default withTheme(Report);
